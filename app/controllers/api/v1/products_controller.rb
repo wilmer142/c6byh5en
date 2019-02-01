7e814b645d77d3652fc1114988df8498604b34e8
wilmer142 <wilmercollege@gmail.com>
2019-01-31 22:01:34 -0500

    Add Patch and Delete routes for products api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -16,6 +16,20 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    if @product.update(product_params)
+      render json: @product, status: 200
+    else
+      render json: {errors: @product.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+    render json: {message: "Producto Borrado"}, status: 204
+  end
+
   private 
 
     def product_params
