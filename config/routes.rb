31f6668a1ccdc0a5b1aeb5cc5752c26129994c3f
wilmer142 <wilmercollege@gmail.com>
2019-01-31 21:08:11 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
 
   namespace :api do
-    namespace :v1, defaults: { format: 'json' } do
+    namespace :v1, defaults: { format: :json } do
       resources :products, only: [:index]
     end 
   end
