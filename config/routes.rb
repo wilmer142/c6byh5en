2b71da0e33f78fc29ef50405d86aa07d1d94f66b
wilmer142 <wilmercollege@gmail.com>
2019-01-31 21:02:19 -0500

    format json route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :products
 
   namespace :api do
-    namespace :v1 do
-      resources :products, only: [:index], :defaults => { :format => 'json' }
+    namespace :v1, defaults: { format: 'json' } do
+      resources :products, only: [:index]
     end 
   end
 
