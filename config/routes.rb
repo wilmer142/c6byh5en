440aa2dfef5e3aaa146195909b9c6e0839277336
wilmer142 <wilmercollege@gmail.com>
2019-01-31 20:46:28 -0500

    Modifying routes setting format json

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :products, only: [:index]
+      resources :products, only: [:index], defaults: {format: 'json'}
     end 
   end
 
